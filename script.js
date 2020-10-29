f68072a3059f9ab439fbbfdc779b82cb38678760
✬ ۞ ☾ Lεηα ☽ ۞ ✬ <45369211+lenatheque@users.noreply.github.com>
2020-10-28 22:26:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -45,13 +45,13 @@ function search(event) {
 function convertToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#fahrenheit-input");
-  temperatureElement.innerHTML = fahrenheitInput;
+  temperatureElement.innerHTML = temperatureElement;
 }
 
 function convertToCelsius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#celsius-input");
-  temperatureElement.innerHTML = celsiusInput;
+  temperatureElement.innerHTML = temperatureElement;
 }
 
 let fahrenheitInput = document.querySelector("#fahrenheit-input");
