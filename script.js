1e48bae746f2c0763ddd52b102f3f4ac359aee7e
✬ ۞ ☾ Lεηα ☽ ۞ ✬ <45369211+lenatheque@users.noreply.github.com>
2020-10-30 00:20:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,34 +23,6 @@ function formatDate(date) {
 }
 
 // FORECAST API
-function displayForecast(response) {
-  let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = null;
-  let forecast = null;
-
-  for (let index = 0; index < 6; index++) {
-    forecast = response.data.list[index];
-    forecastElement.innerHTML += `
-      <div class="col-2">
-        <h3>
-          ${formatHours(forecast.dt * 1000)}
-        </h3>
-        <img
-          src="http://openweathermap.org/img/wn/${
-            forecast.weather[0].icon
-          }@2x.png"
-        />
-        <div class="weather-forecast-temperature">
-          <strong>
-          ${Math.round(forecast.main.temp_max)}°
-          </strong> 
-          ${Math.round(forecast.main.temp_min)}°
-        </div>
-    </div>
-    `;
-  }
-}
-
 ///
 function displayWeatherCondition(response) {
   console.log(response.data);
