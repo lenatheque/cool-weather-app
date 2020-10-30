378cec3f43fcce38febe1769769107fd92701623
✬ ۞ ☾ Lεηα ☽ ۞ ✬ <45369211+lenatheque@users.noreply.github.com>
2020-10-30 00:20:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function displayWeatherCondition(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  // iconElement.setAttribute("alt", response.data.weather[0].description);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let iconElement = document.querySelector("#icon-today");
