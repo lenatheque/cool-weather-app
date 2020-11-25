4f7729da362fd0a92d4a506d0721454c0f4427f9
✬ ۞ ☾ Lεηα ☽ ۞ ✬ <45369211+lenatheque@users.noreply.github.com>
2020-11-24 21:03:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function displayForecast(response) {
   forecastElement.innerHTML = null;
   let forecast = null;
 
-  for (let index = 0; index < 5; index++) {
+  for (let index = 0; index < 12; index++) {
     forecast = response.data.list[index];
     forecastElement.innerHTML += `
     <div class="col-2">
