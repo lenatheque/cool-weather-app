00c720ee25f6e18c4a6371ad758dad9d16740597
✬ ۞ ☾ Lεηα ☽ ۞ ✬ <45369211+lenatheque@users.noreply.github.com>
2020-11-24 21:01:23 -0500

    adding H2 in JS

--- script.js
+++ script.js
@@ -58,10 +58,11 @@ function displayForecast(response) {
   forecastElement.innerHTML = null;
   let forecast = null;
 
-  for (let index = 0; index < 7; index++) {
+  for (let index = 0; index < 5; index++) {
     forecast = response.data.list[index];
     forecastElement.innerHTML += `
     <div class="col-2">
+    <h2>Hourly Forecast</h2>
       <h3>
         ${formatHours(forecast.dt * 1000)}
       </h3>
